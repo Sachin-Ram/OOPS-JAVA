b2dbdc6f67d198a54bfbf8a3b5dda9113a0515a1
Sachin-Ram <112873304+Sachin-Ram@users.noreply.github.com>
2022-09-05 19:57:10 +0530

    Update Speed Of Light Travel.java

--- exp 1/Speed Of Light Travel.java
+++ exp 1/Speed Of Light Travel.java
@@ -1,14 +1,17 @@
 
-package demo;
+package exp1;
+import java.util.scanner;
 
-public class Demo {
+public class speed {
 
    
    public static void main(String[] args) {
+      Scanner o=new Scanner (System.in);
       int speed;
       long days, seconds, dist;
       speed = 186000;
-      days = 365;
+      System.out.println("enter the number of days");
+      long days=o.nextLong();
       seconds = days * 24 * 60 * 60;
       dist = speed * seconds;
       System.out.print("Light travels: "+dist + " miles");
