a0a2d66878f74fa622023f4429c07a09ffd1a7ea
Sachin-Ram <112873304+Sachin-Ram@users.noreply.github.com>
2022-09-05 19:39:48 +0530

    Update Speed Of Light Travel.java

--- exp 1/Speed Of Light Travel.java
+++ exp 1/Speed Of Light Travel.java
@@ -1,18 +1,8 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package demo;
 
-/**
- *
- * @author 21cse104
- */
 public class Demo {
 
-    /**
-     * @param args the command line arguments
-     */
    
    public static void main(String[] args) {
       int speed;
